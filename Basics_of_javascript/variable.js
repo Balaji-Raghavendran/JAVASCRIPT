5aa5114796fb4694e8fd06f97ebc9dc52dae7dde
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-08-30 23:38:13 +0530

    variable javascript

---
+++ Basics_of_javascript/variable.js
@@ -0,0 +1,8 @@
+// write a program to print product name, price and the tax
+
+var product = "bluetooth earphones"
+var price = 5000
+var tax = 7.5
+var total = (price*tax)
+console.log(product)
+console.log(total)
