de422813ed2a710ecddab38ca0907cb4a4fae30f
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-08-31 13:25:31 +0530

    datatypes JavaScript

---
+++ Basics_of_javascript/datatypes.js
@@ -0,0 +1,17 @@
+// program to identify the dataypes for each code
+
+var a = 10
+console.log(typeof(a))
+
+var a = "javascript"
+console.log(typeof(a))
+
+var a = true
+console.log(typeof(a))
+
+var a = null
+console.log(typeof(a))
+
+var a 
+console.log(typeof(a))
+
