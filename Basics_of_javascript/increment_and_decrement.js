4ea83c0a3ff0edfd4fdd95da69ca50cbe434896e
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-08-31 13:08:08 +0530

    increment and decrement JavaScript

---
+++ Basics_of_javascript/increment_and_decrement.js
@@ -0,0 +1,24 @@
+// program for post incrementing and post decrementing the given values
+
+var a = 10
+var b = a++
+console.log(b)
+console.log(a)
+
+var a = 10
+var b = a--
+console.log(b)
+console.log(a)
+
+
+// program for pre incrementing and pre decrementing the given values
+
+var a = 10
+var b = ++a
+console.log(b)
+console.log(a)
+
+var a = 20
+var b = --a
+console.log(b)
+console.log(a)
