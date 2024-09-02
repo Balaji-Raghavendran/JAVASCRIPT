cfcb5a87877476a6dfc1722df12a4c0a8dd26adc
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-09-02 22:00:04 +0530

    If Else JavaScript

---
+++ Basics_of_javascript/if_else.js
@@ -0,0 +1,81 @@
+// program for if else conditional statements in JavaScript
+rain = true
+if(rain)
+{
+    console.log("Take an umbrella")
+}
+else
+{
+    console.log("enjoy the sunshine")
+}
+
+// another if else example code
+
+var season = "summer"
+
+if(season=="spring")
+{
+    console.log("enjoy the blooming flowers")
+}
+
+if(season=="summer")
+{
+    console.log("Have fun in summer")
+}
+
+if(season=="Autumn")
+{
+    console.log("Admire the colourful leaves")
+}
+
+if(season=="winter")
+{
+    console.log("Bundle up and stay warm")
+}
+
+// another example program
+
+var score = 80
+
+if(score<50)
+{
+    console.log("you need to improve")
+}
+
+if(score>50 && score<70)
+{
+    console.log("Good job")
+}
+
+if(score>70)
+{
+    console.log("Excellent Work")
+}
+
+// another example program
+
+var num = 10
+
+if(num%2==0)
+{
+    console.log("It is even")
+}
+
+else
+{
+    console.log("It is odd")
+}
+
+// another example program
+
+var char = "a,e,i,o,u"
+
+if(char=="a,e,i,o,u")
+{
+    console.log("It is a vowel")
+}
+
+else
+{
+    console.log("It is a consonant")
+}
