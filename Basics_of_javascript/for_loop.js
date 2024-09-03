bee97ab6ac9418c8e4ef81d3c86f94f4499e0b05
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-09-03 22:49:44 +0530

    For Loop JavaScript

---
+++ Basics_of_javascript/for_loop.js
@@ -0,0 +1,15 @@
+// program in for loop JavaScript
+
+for(let i=1;i<6;i++)
+{
+    console.log(i)
+}
+
+//another example code
+
+const fruits = ["apple","banana","mango"]
+
+for(i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i])
+}
