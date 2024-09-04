6025e25ba862e7c832ec878dcf18d5d87ae9c964
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-09-04 09:42:18 +0530

    For Loop 2 JavaScript

---
+++ Basics_of_javascript/for_loop_2.js
@@ -0,0 +1,28 @@
+//another example program in for loop JavaScript
+
+
+for(count=1;count<=10;count=count+1)
+{
+    console.log(count)
+}
+
+//another example program
+
+for(count=1;count<=10;count=count+2)
+{
+    console.log(count)
+}
+
+//print even numbers using for loop
+
+for(count=2;count<=10;count=count+2)
+{
+    console.log(count)
+}
+
+//print 2 tables using for loop
+
+for(count=1;count<=10;count=count+1)
+{
+    console.log("2X"+count+"="+count*2)
+}
